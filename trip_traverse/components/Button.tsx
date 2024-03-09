455469a6ee1d2ccb4febc083dd3217774752d73b
Palak <palak.desai4501@gmail.com>
2024-03-09 12:49:18 -0500

    Feature: Accepted Props and gave ButtonProps type

--- trip_traverse/components/Button.tsx
+++ trip_traverse/components/Button.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: 'btn_dark_green'
+}
 
-const Button = () => {
+const Button = ({type, title, icon, variant}: ButtonProps) => {
   return (
-    <div>
-      Button
-    </div>
+    <button type="button">
+      Log In
+    </button>
   )
 }
 
