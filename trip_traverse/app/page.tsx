3e49e421dc4be1be9eaa7cc9dc47a51afc22dbf7
Palak <palak.desai4501@gmail.com>
2024-03-09 12:13:29 -0500

    Feature: Added changes

--- trip_traverse/app/page.tsx
+++ trip_traverse/app/page.tsx
@@ -1,8 +1,27 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import type { Metadata } from 'next'
+import Footer from '@/components/Footer'
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'TripTraverse',
+  description: 'Travel App for Camping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">
-      Hello World
-    </h1>
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
   )
 }
