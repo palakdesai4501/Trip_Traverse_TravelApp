05f437c7af7e3c7100f39c31dce1cf2b4dec4f0d
Palak <palak.desai4501@gmail.com>
2024-03-10 17:39:44 -0400

    Feature: Added Campsite component and assigned CampProps type

--- trip_traverse/components/Camp.tsx
+++ trip_traverse/components/Camp.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+interface CampProps{
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return <div>Camp Site 1</div>;
+};
 
 const Camp = () => {
   return (
-    <div>
-      Camp
+    <div className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Camp
+export default Camp;
