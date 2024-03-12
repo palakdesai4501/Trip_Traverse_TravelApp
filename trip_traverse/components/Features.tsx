f5b8c07e232b4e35c4705230532a1414b330cd14
Palak <palak.desai4501@gmail.com>
2024-03-11 23:24:32 -0400

    Feature: Added description

--- trip_traverse/components/Features.tsx
+++ trip_traverse/components/Features.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <section className='border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-      <div className='max-container padding-container relative w-full justify-end'>
+    <section className='flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+      <div className='max-container padding-container relative w-full flex justify-end'>
         <div className='flex flex-1 lg:min-h-[900px]'>
           <Image 
             src="/phone.png"
@@ -27,13 +27,12 @@ const Features = () => {
             />
             <h2 className='bold-40 lg:bold-64'>Our Features</h2>
           </div>
-          <ul>
+          <ul className='mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20'>
             {FEATURES.map((feature) => (
               <FeatureItem 
               key={feature.title}
               title={feature.title}
               icon={feature.icon}
-              variant={feature.variant}
               description={feature.description}/>
             ))}
           </ul>
@@ -55,6 +54,12 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
       <div className='rounded-full p-4 lg:p-7 bg-green-50'>
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
+      <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>
+        {title}
+      </h2>
+      <p className='regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[39px] lg:bg-none'>
+        {description}
+      </p>
     </li>
   )
 }
